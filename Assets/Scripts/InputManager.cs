ba6b2a432eae82aeb3f34aeab21aaad77ddd5ca1
Maxim Alexeenko <max.alekseenko@gmail.com>
2021-04-28 01:35:59 +0300

    place model (tut 4)

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject arObject;
+    [SerializeField] private Camera arCamera;
+    [SerializeField] private ARRaycastManager raycastManager;
+
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
+            if (raycastManager.Raycast(ray, hits))
+            {
+                Pose pose = hits[0].pose;
+                Instantiate(arObject, pose.position, pose.rotation);
+            }
+        }
+    }
+}
