d2148ddfc7a967db468a2060bb675b42471949d1
Maxim Alexeenko <max.alekseenko@gmail.com>
2021-04-27 18:33:36 +0300

    select floor(tut 3)

---
+++ Assets/Scripts/PlaneMaterialManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneMaterialManager : MonoBehaviour
+{
+    public Material planeMaterial;
+    public Button[] planeTextureButtons;
+
+    void Awake() {
+        foreach (var button in planeTextureButtons) {
+            Texture texture = button.transform.Find("Mask/RawImage").GetComponent<RawImage>().texture;
+            button.onClick.AddListener(()=>OnClickButton(texture));
+        }
+    }
+
+    void Update(){
+        
+    }
+
+    void OnClickButton(Texture texture) {
+        planeMaterial.mainTexture = texture;
+    }
+}
