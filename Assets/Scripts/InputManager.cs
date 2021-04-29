011c8ef080a38b531ca22b87b52fd96af8ea1e37
Maxim Alexeenko <max.alekseenko@gmail.com>
2021-04-29 04:52:58 +0300

    model and ui click fix

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -10,17 +11,31 @@ public class InputManager : MonoBehaviour
 
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
+    Touch touch;
+
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        touch = Input.GetTouch(0);
+
+        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began) 
+            return;
+        if (IsPointerOverUI(touch)) 
+            return;
+
+        Ray ray = arCamera.ScreenPointToRay(touch.position);
+        if (raycastManager.Raycast(ray, hits))
         {
-            Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
-            if (raycastManager.Raycast(ray, hits))
-            {
-                Pose pose = hits[0].pose;
-                Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
-            }
+            Pose pose = hits[0].pose;
+            Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
         }
+    }
+    bool IsPointerOverUI(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
     }
 }
